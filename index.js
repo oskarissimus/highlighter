19736f2ce2f4051fcc0149c0ca70b34c51f3f2a2
Oskar Korczak <oskar.korczak@protonmail.com>
2023-08-21 17:58:21 +0200

    chore: Remove unused files, functions, and dependencies

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const vsctm = require('vscode-textmate');
-const oniguruma = require('vscode-oniguruma');
+const Parser = require('tree-sitter');
+const JavaScript = require('tree-sitter-python');
 
-function readFile(path) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, (error, data) => error ? reject(error) : resolve(data));
-    })
-}
+const parser = new Parser();
+parser.setLanguage(JavaScript);
+const sourceCode = `
+def foo():
+    print("Hello World")
 
-const wasmBin = fs.readFileSync(path.join(__dirname, './node_modules/vscode-oniguruma/release/onig.wasm')).buffer;
-const vscodeOnigurumaLib = oniguruma.loadWASM(wasmBin).then(() => {
-    return {
-        createOnigScanner(patterns) { return new oniguruma.OnigScanner(patterns); },
-        createOnigString(s) { return new oniguruma.OnigString(s); }
-    };
-});
-
-const registry = new vsctm.Registry({
-    onigLib: vscodeOnigurumaLib,
-    loadGrammar: (scopeName) => {
-        if (scopeName === 'source.python') {
-            return readFile('./Python.tmLanguage').then(data => vsctm.parseRawGrammar(data.toString()))
-        }
-        console.log(`Unknown scope name: ${scopeName}`);
-        return null;
-    }
-});
-
-function tokenizeGrammar(grammar) {
-    const text = [
-        `def sayHello(name):`,
-        `    return "Hello, " + name`,
-        `sayHello("World")`
-    ];
-    let ruleStack = vsctm.INITIAL;
-    for (let i = 0; i < text.length; i++) {
-        const line = text[i];
-        const lineTokens = grammar.tokenizeLine(line, ruleStack);
-        console.log(`\nTokenizing line: ${line}`);
-        for (let j = 0; j < lineTokens.tokens.length; j++) {
-            const token = lineTokens.tokens[j];
-            console.log(` - token from ${token.startIndex} to ${token.endIndex} ` +
-                `(${line.substring(token.startIndex, token.endIndex)}) ` +
-                `with scopes ${token.scopes.join(', ')}`
-            );
-        }
-        ruleStack = lineTokens.ruleStack;
-    }
-}
-
-registry.loadGrammar('source.python').then(tokenizeGrammar);
+foo()
+`;
+const tree = parser.parse(sourceCode);
+console.log(tree.rootNode.toString());
--- papiez/config-overrides.js
+++
@@ -1,9 +0,0 @@
-module.exports = function override(config, env) {
-    // Add a loader for .tmLanguage files
-    config.module.rules.push({
-        test: /\.tmLanguage$/,
-        type: 'asset/source'
-    });
-
-    return config;
-};
