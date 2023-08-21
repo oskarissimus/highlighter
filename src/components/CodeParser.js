653b5af5ed66bcff10e6231da1869c6bf5bd4ed8
Oskar Korczak <oskar.korczak@protonmail.com>
2023-08-21 19:57:26 +0200

    refactor: Refactor code parser imports and remove unnecessary hooks

--- src/components/CodeParser.js
+++ src/components/CodeParser.js
@@ -1,32 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useCodeParser } from '../hooks/use-code-parser';
 
-const useCodeParser = (sourceCode) => {
-    const [parsedOutput, setParsedOutput] = useState('');
-    const [parser, setParser] = useState(null);
-    const [language, setLanguage] = useState(null);
-
-    useEffect(() => {
-        // Initialize the web-tree-sitter library
-        const Parser = require('web-tree-sitter');
-        Parser.init().then(async () => {
-            const newParser = new Parser();
-            setParser(newParser);
-
-            const PythonLanguage = await Parser.Language.load('./tree-sitter-python.wasm');
-            newParser.setLanguage(PythonLanguage);
-            setLanguage(PythonLanguage);
-        });
-    }, []);
-
-    useEffect(() => {
-        if (parser && language) {
-            const tree = parser.parse(sourceCode);
-            setParsedOutput(tree.rootNode.toString());
-        }
-    }, [sourceCode, parser, language]);
-
-    return parsedOutput;
-};
 
 
 const CodeParser = ({ children }) => {
---
+++ src/hooks/use-code-parser.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+export const useCodeParser = (sourceCode) => {
+    const [parsedOutput, setParsedOutput] = useState('');
+    const [parser, setParser] = useState(null);
+    const [language, setLanguage] = useState(null);
+
+    useEffect(() => {
+        // Initialize the web-tree-sitter library
+        const Parser = require('web-tree-sitter');
+        Parser.init().then(async () => {
+            const newParser = new Parser();
+            setParser(newParser);
+
+            const PythonLanguage = await Parser.Language.load('./tree-sitter-python.wasm');
+            newParser.setLanguage(PythonLanguage);
+            setLanguage(PythonLanguage);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (parser && language) {
+            const tree = parser.parse(sourceCode);
+            setParsedOutput(tree.rootNode.toString());
+        }
+    }, [sourceCode, parser, language]);
+
+    return parsedOutput;
+};
